fc67f5bb247aca293230214a96e16f5e538591ab
Toru Shiozaki <shiozaki.toru@gmail.com>
2012-03-18 16:11:13 +0100

    The GPL header included to each file.

--- src/pretensor.h
+++ src/pretensor.h
@@ -1,7 +1,28 @@
 //
-// Author : Toru Shiozaki
-// Date   : Feb 2009
+// SMITH2 - Automated Code Generation Program for General Order CC Programs.
+// Filename: pretensor.h
+// Copyright (C) 2009 Toru Shiozaki
 //
+// Author: Toru Shiozaki <shiozaki.toru@gmail.com>
+// Maintainer: Shiozaki group
+//
+// This file is part of the SMITH2 package (to be renamed).
+//
+// The SMITH2 package is free software; you can redistribute it and\/or modify
+// it under the terms of the GNU Library General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// The SMITH2 package is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with the SMITH2 package; see COPYING.  If not, write to
+// the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
 #ifndef _smith_pretensor_h
 #define _smith_pretensor_h
 
